fbab76e905725f95782977f73efe9a9e30a21f0c
yoshitake1014 <yoshitake10122@gmail.com>
2022-12-13 18:33:55 +0000

    #

--- models/lmdgnn.py
+++ models/lmdgnn.py
@@ -4,102 +4,147 @@ from torch import nn
 
 
 class Encoder(nn.Module):
-    def __init__(self,
-                input_size,
-                output_size):
-        super(Encoder,self).__init__()
+    def __init__(self, input_size, output_size):
+        super(Encoder, self).__init__()
+
         self.encoder = nn.Sequential(
                 nn.Linear(input_size, 500),
                 nn.ReLU(),
-                nn.Linear(500,300),
+                nn.Linear(500, 300),
                 nn.ReLU(),
-                nn.Linear(300,output_size),
+                nn.Linear(300, output_size),
                 nn.ReLU()
                 )
 
 
-    def forward(self,x):
+    def forward(self, x):
         return self.encoder(x)
 
 
 class Decoder(nn.Module):
-    def __init__(self,
-                input_size,
-                output_size):
-        super(Decoder,self).__init__()
+    def __init__(self, input_size, output_size):
+        super(Decoder, self).__init__()
+
         self.decoder = nn.Sequential(
-                nn.Linear(input_size,300),
+                nn.Linear(input_size, 300),
                 nn.ReLU(),
-                nn.Linear(300,500),
+                nn.Linear(300, 500),
                 nn.ReLU(),
-                nn.Linear(500,output_size),
+                nn.Linear(500, output_size),
                 nn.Sigmoid()
                 )
 
 
-    def forward(self,x):
+    def forward(self, x):
         return self.decoder(x)
 
 
-class Autoencoder(nn.Module):
-    def __init__(self,
-                num_nodes,
-                emb_size):
-        super(Autoencoder,self).__init__()
-        self.enc = Encoder(num_nodes,emb_size)
-        self.dec = Decoder(emb_size,num_nodes)
+class MLSTMCell(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(MLSTMCell, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.lag_k = 100
+
+        self.d_gate = nn.Linear(input_size+hidden_size, input_size)
+        self.i_gate = nn.Linear(input_size+hidden_size, input_size)
+        self.o_gate = nn.Linear(input_size+hidden_size, input_size)
+        self.c_gate = nn.Linear(input_size+hidden_size, input_size)
+
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
 
-    def forward(self,x):
-        x = self.enc(x)
-        x = self.dec(x)
+    def forward(self, x, hidden_state, memory_cell):
+        if hidden_state is None:
+            hidden_state = torch.zeros(1, self.hidden_size)
+        if memory_cell is None:
+            memory_cell = torch.zeros(1, self.hidden_size)
+
+        cat = torch.cat((x, hidden_state), 1)
+        d = self.d_gate(cat)
+        d = self.sigmoid(d) * 0.5
+        i = self.i_gate(cat)
+        i = self.sigmoid(i)
+        o = self.o_gate(cat)
+        o = self.sigmoid(o)
+        c_tilde = self.c_gate(cat)
+        c_tilde = self.tanh(c_tilde)
+
+        return
+
+
+class MLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(MLSTM, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.hidden_state = None
+        self.memory_cell = None
+        self.mlstm_cell = MLSTMCell(self.input_size, self.hidden_size)
+
+
+    def forward(self, x):
+        x, self.hidden_state, self.memory_cell =\
+        self.mlstm_cell(x, self.hidden_state, self.memory_cell)
         return x
 
 
 class LMDGNN(nn.Module):
-    def __init__(self,
-                args,
-                num_nodes,
-                emb_size):
-        super(LMDGNN,self).__init__()
-        self.autoencoder = Autoencoder(num_nodes,emb_size)
+    def __init__(self, args, num_nodes, emb_size):
+        super(LMDGNN, self).__init__()
+
+        self.enc = Encoder(num_nodes, emb_size)
+        self.dec = Decoder(emb_size, num_nodes)
+        self.mlstm = MLSTM(emb_size, emb_size)
 
 
-    def forward(self,x):
-        return self.autoencoder(x)
+    def forward(self, x):
+        x = self.enc(x)
+        #x = self.mlstm(x)
+        x = self.dec(x)
+        return x
 
 
 def train(dataloader, model, loss_fn, optimizer):
     THRESHOLD = 0.5
 
     model.train()
+
     for X,y in dataloader:
-        X,y = X.to('cpu'), y.to('cpu')
+        X, y = X.to('cpu'), y.to('cpu')
 
         pred = model(X)
         threshold = torch.Tensor([THRESHOLD])
-        pred = (pred >= threshold).int()
-        loss = loss_fn(pred,y)
+        pred = (pred >= threshold).type(torch.int)
+        loss = loss_fn(pred, y)
         loss.requires_grad = True
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    print(f'loss: {loss}')
+    print(f'Loss: {loss}')
 
 
-def test(dataloader, model):
+def test(dataloader, model, loss_fn):
     THRESHOLD = 0.5
 
     model.eval()
+
+    correct = 0
+    size = 0
     with torch.no_grad():
         for X,y in dataloader:
-            X,y = X.to('cpu'), y.to('cpu')
+            X, y = X.to('cpu'), y.to('cpu')
 
             pred = model(X)
-            threshold = torch.Tensor([0.5])
-            pred = (pred >= threshold).int()
-            correct = (pred == y).type(torch.int).sum()
-            accuracy = correct / pred.numel()
-            print(f'Accuracy: {accuracy}')
+            threshold = torch.Tensor([THRESHOLD])
+            pred = (pred >= threshold).type(torch.int)
+            correct += (pred == y).type(torch.float).sum()
+            size += pred.numel()
+    correct /= size
+
+    print(f'Accuracy: {100*correct}')
--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 
 from models import lmdgnn, dynaernn, dynrnn, dynae, dyngem
+from utils import loss_function as lf
 
 
 parser = argparse.ArgumentParser()
@@ -19,7 +20,7 @@ args = parser.parse_args()
 def main():
     dataset = args.datasets
     if dataset == 'as':
-        BATCH_SIZE = 50
+        BATCH_SIZE = 1
         EMB_SIZE = 150
         NUM_NODES = 7716
         LEARNING_RATE = 1e-3
@@ -31,7 +32,6 @@ def main():
         listdir = os.listdir('datasets/as_733')
         listdir.sort()
         graphs = []
-        #for i in range(len(listdir)):
         for i in range(TIME_STEP):
             G = nx.read_gpickle(f'datasets/as_733/month_{i+1}_graph.gpickle')
             graphs.append(G)
@@ -42,42 +42,55 @@ def main():
 
     method = args.methods
     if method == 'lmdgnn':
-        model = lmdgnn.LMDGNN(args,NUM_NODES,EMB_SIZE)
+        model = lmdgnn.LMDGNN(args, NUM_NODES, EMB_SIZE)
 
-        for i in range(TIME_STEP-1):
+        for i in range(TIME_STEP-2):
             graph = graphs[i]
             next_graph = graphs[i+1]
+            test_graph = graphs[i+2]
 
             G_cen = nx.degree_centrality(graph)
             G_cen = sorted(G_cen.items(), key=lambda item:item[1], reverse=True)
 
             # Training
-            sample_nodes = []
-            sample_next_nodes = []
+            nodes = []
+            next_nodes = []
+            test_nodes = []
             for j in range(SAMPLE_SIZE):
                 x = [0]*NUM_NODES
                 y = [0]*NUM_NODES
-                z = G_cen[j][0]
-                neighbors_1 = graph.neighbors(z)
-                neighbors_2 = next_graph.neighbors(z)
+                z = [0]*NUM_NODES
+                node = G_cen[j][0]
+                neighbors_1 = graph.neighbors(node)
+                neighbors_2 = next_graph.neighbors(node)
+                neighbors_3 = test_graph.neighbors(node)
                 for k in neighbors_1:
                     x[k-1] = 1
-                sample_nodes.append(x)
+                nodes.append(x)
                 for k in neighbors_2:
                     y[k-1] = 1
-                sample_next_nodes.append(y)
+                next_nodes.append(y)
+                for k in neighbors_3:
+                    z[k-1] = 1
+                test_nodes.append(z)
 
-            X = torch.Tensor(sample_nodes)
-            y = torch.Tensor(sample_next_nodes)
-            train_dataset = TensorDataset(X,y)
+            X = torch.Tensor(nodes)
+            Y = torch.Tensor(next_nodes)
+            Z = torch.Tensor(test_nodes)
+            train_dataset = TensorDataset(X,Y)
+            test_dataset = TensorDataset(Y,Z)
             train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+            test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
-            loss_fn = nn.MSELoss()
-            optimizer = torch.optim.SGD(model.parameters(),lr=LEARNING_RATE)
+            #loss_fn = nn.MSELoss()
+            loss_fn = lf.WeightedMSELoss()
+            optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
             lmdgnn.train(train_dataloader, model, loss_fn, optimizer)
 
             # Testing
+            lmdgnn.test(test_dataloader, model, loss_fn)
+
             print('-'*50)
     elif method == 'dyngem':
         pass
