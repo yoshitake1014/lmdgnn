c572e5898f5df64d6a118f6bc3f528efe6e31e97
yoshitake1014 <yoshitake10122@gmail.com>
2023-01-06 02:59:31 +0000

    #

--- utils/setup_as_caida.py
+++ utils/setup_as_caida.py
@@ -5,17 +5,17 @@ import networkx as nx
 
 routers = {}
 router_id = 0
-listdir = sorted(os.listdir('../datasets/as-733'))
+listdir = sorted(os.listdir('../datasets/as-caida'))
 file_no = 1
 
 for i in listdir:
-    with open(f'../datasets/as-733/{i}') as f:
-        G = nx.Graph()
-        G.add_nodes_from(range(7716))
+    with open(f'../datasets/as-caida/{i}') as f:
+        G = nx.DiGraph()
+        G.add_nodes_from(range(31379))
         for l in f:
             l = l.strip()
             if l.startswith('#'): continue
-            node_i,node_j = l.split('\t')
+            node_i,node_j, _ = l.split('\t')
             if node_i not in routers:
                 routers[node_i] = router_id
                 router_id += 1
@@ -23,6 +23,6 @@ for i in listdir:
                 routers[node_j] = router_id
                 router_id += 1
             G.add_edge(routers[node_i],routers[node_j],weight=1)
-        file_name = f'../datasets/as_733/month_{file_no}_graph.gpickle'
+        file_name = f'../datasets/as_caida/{file_no}_graph.gpickle'
         nx.write_gpickle(G,file_name)
         file_no += 1
